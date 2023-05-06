60842c0d3847f2749af3e73c90dd1bf451f04224
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-05-06 17:50:34 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  let actorsList = actors.map((actor,index)=>{
+    return <div key = {index}>
+      <h2>{actor.name}</h2>
+      <ul>
+        {actor.movies.map((movie,index)=>{
+          return <li key = {index}>{movie}</li>
+        })}
+      </ul>
+    </div>
+  })
+  return <div>
+    <h1>Actors Page</h1>
+    {actorsList}
+    </div>;
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  let movieList = movies.map((movie,index)=>{
+    return(
+      <div key={index}>
+        <h2>{movie.title}</h2>
+        <h2>{movie.time}</h2>
+        <ul>
+          {movie.genres.map((genre,index)=>{
+            return <li key={index}>{genre}</li>
+          })}
+        </ul>
+      </div>
+    )
+  })
+  return <div>
+    <h1>Movies Page</h1>
+    {movieList}
+  </div>;
 }
 
 export default Movies;
